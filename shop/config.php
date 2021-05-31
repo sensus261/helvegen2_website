d13f0c94397d6ab73f82a6cbc0f3bc5408236b62
Sensus <dorin.alexandru.dragan@gmail.com>
2021-05-31 19:32:56 +0300

    item shop config init

---
+++ shop/config.php
@@ -0,0 +1,19 @@
+<?php
+	//Details database
+	$host = "195.178.102.115";
+	$user  = "root";
+	$password =  "MYSQLSensus261970";
+	
+	//Site URL - with / at the end, example: http://example.com/ishop/
+	$site_url = "https://localhost/helvegen2_website/shop";
+	
+	//General settings
+	$server_name = "Metin2CMS";
+	$minim_web_admin_level = 9;
+	
+	//PayPal email - only Business Account
+	$paypal_email = "demo@metin2cms.cf";
+	
+	//No edit
+	$license = "4VXC6-3WSXL-W55RP-AUKZ2";
+?>
